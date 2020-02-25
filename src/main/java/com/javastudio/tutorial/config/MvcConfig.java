c050ccc8d3694f99988ac7b8c703102ca64632b6
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-02-25 16:15:13 +0330

    Spring Security

--- src/main/java/com/javastudio/tutorial/config/MvcConfig.java
+++ src/main/java/com/javastudio/tutorial/config/MvcConfig.java
@@ -3,15 +3,12 @@ package com.javastudio.tutorial.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = {"com.javastudio.tutorial"})
-public class WebMvcConfig implements WebMvcConfigurer {
+public class MvcConfig {
     @Bean
     public InternalResourceViewResolver resolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,9 +1,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Home</title>
+    <title>Login</title>
 </head>
 <body>
-<h1>Hello</h1>
+<h1>Login</h1>
 </body>
 </html>
