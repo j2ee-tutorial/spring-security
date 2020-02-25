1ce91d35d7400371265b1f9d1b66880c98a35c70
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-02-25 17:05:08 +0330

    Spring Security

--- src/main/java/com/javastudio/tutorial/config/SecurityWebApplicationInitializer.java
+++ src/main/java/com/javastudio/tutorial/config/SecurityWebApplicationInitializer.java
@@ -2,8 +2,13 @@ package com.javastudio.tutorial.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+import java.util.logging.Logger;
+
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    Logger logger = Logger.getLogger(SecurityWebApplicationInitializer.class.getName());
+
     public SecurityWebApplicationInitializer() {
         super(SecurityConfig.class);
+        logger.info("Initializing Filter for Spring Security");
     }
 }
