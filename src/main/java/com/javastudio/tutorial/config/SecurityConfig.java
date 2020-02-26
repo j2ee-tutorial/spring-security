0b48ff083b6185024bcec1a9702653b221af772f
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-02-26 11:53:31 +0330

    Spring security

--- src/main/java/com/javastudio/tutorial/config/SecurityConfig.java
+++ src/main/java/com/javastudio/tutorial/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.javastudio.tutorial.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -36,11 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/login*").permitAll()
                 .antMatchers("/**").hasAnyRole("ADMIN", "USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/login")
-                .and().csrf().disable()
-        ;
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/authenticate");
+
+        http.csrf().disable();
     }
 
     @Bean
